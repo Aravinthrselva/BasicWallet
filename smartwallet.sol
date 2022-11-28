a5e00e327ee11de17b23dd8cf8acf41212300d26
Aravinth Selva <aravinthsraju@gmail.com>
2022-11-28 21:15:29 +0530

    Update smartwallet.sol

--- smartwallet.sol
+++ smartwallet.sol
@@ -8,7 +8,7 @@ pragma solidity ^0.8.16;
 3.It is possible for the owner to spend funds on any kind of address, no matter if its a Externally Owned Account (EOA - with a private key), 
 or a Contract Address.
 4.It should be possible to allow certain people to spend up to a certain amount of funds.
-5.It should be possible to set the owner to a different address by a minimum of 3 out of 5 guardians, in case funds are lost.
+5.It should be possible to set the owner to a different address by a minimum of 3 voters, in case funds are lost.
 */
 
 
